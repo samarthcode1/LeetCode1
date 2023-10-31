02e9be9145cc3e3cef0c83cff5c52e01d1c2526e
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2023-10-31 10:12:15 +0530

    Time: 66 ms (73.17%), Space: 59.1 MB (77.61%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        // int max=INT_MIN;
+        // for(int i=0;i<height.size();i++){
+        //     for(int j=i+1;j<height.size();j++){
+        //         int diff=abs(j-i);
+        //         int min;
+        //         if(height[i]<height[j]){
+        //             min=height[i];
+        //         }
+        //         else{
+        //             min=height[j];
+        //         }
+        //         // cout<<min<<" ";
+        //         int amount=min*diff;
+        //         if(max<amount){
+        //             max=amount;
+        //         }
+        //     }
+        // }
+        // return max;
+        int max=INT_MIN;
+        int i=0,j=height.size()-1;
+            while(i<j){
+                int diff=abs(j-i);
+                int mini;
+                mini=min(height[i],height[j]);
+                // if(height[i]<height[j]){
+                //     min=height[i];
+                // }
+                // else{
+                //     min=height[j];
+                // }
+                // cout<<min<<" ";
+                int amount=mini*diff;
+                if(height[i]>height[j]){
+                    j--;
+                }
+                else{
+                    i++;
+                }
+                if(max<amount){
+                    max=amount;
+                }
+            }
+        return max;
+    }
+};
