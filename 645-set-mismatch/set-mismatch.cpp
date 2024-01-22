d7182f7d82310e25cebd6b164b5cfef89e2010e6
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-01-22 11:01:32 +0530

    Time: 82 ms (11.23%) | Memory: 46.3 MB (8.31%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int>v;
+        sort(nums.begin(),nums.end());
+        unordered_map<int,int>mp;
+        for(auto i:nums){
+            mp[i]++;
+        }
+        for(auto i:mp){
+            if(i.second>1){
+                v.push_back(i.first);
+                break;
+            }
+        }
+        set<int>st;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            st.insert(nums[i]);
+        }
+        set<int>::iterator itr;
+        itr=st.begin();
+        for(int i=0;i<n;i++){
+            if(*itr!=i+1){
+                v.push_back(i+1);
+                break;
+            }
+            itr++;
+        }
+        return v;
+    }
+};
