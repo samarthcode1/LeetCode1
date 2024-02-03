d0405f0f52aaf0097b5432c233b832de6d029f62
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-02-03 18:57:07 +0530

    Time: 4 ms (96.87%) | Memory: 10.7 MB (28.35%) - LeetSync

---
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dp[501];
+    int recurr(vector<int>& arr, int k,int i,int n){
+        if(i>=n){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int maxVal=0;
+        int maxi=0;
+        // int len=0;
+
+        for(int j=i;j<min(n,i+k);j++){
+            // len++;
+            maxVal=max(maxVal,arr[j]);
+            maxi=max(maxi,(j-i+1)*maxVal+recurr(arr,k,j+1,n));
+        }
+        dp[i]=maxi;
+        return dp[i];
+    }
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        memset(dp,-1,sizeof(dp));
+        int n=arr.size();
+        return recurr(arr,k,0,n);
+    }
+};
