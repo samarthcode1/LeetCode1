cd9eb509049e66ec435640a594a576dd15704dda
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-02-25 18:40:42 +0530

    Time: 10 ms (16.83%) | Memory: 18.3 MB (45.24%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -12,40 +12,89 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
 
-        ListNode* p1=list1;
-        ListNode* p2=list2;
-        ListNode* temp= new ListNode(-1);
-        ListNode* p3=temp;
+    //     ListNode* temp= new ListNode(-1);
+        // ListNode* p1=list1;
+        // ListNode* p2=list2;
+    //     ListNode* p3=temp;
 
-        while(p1!=NULL && p2!=NULL){
-            if(p1->val<p2->val){
-                p3->next=p1;
-                p1=p1->next;
-            }
+    //     while(p1!=NULL && p2!=NULL){
+    //         if(p1->val<p2->val){
+    //             p3->next=p1;
+    //             p1=p1->next;
+    //         }
 
-            else{
-                p3->next=p2;
-                p2=p2->next;
-            }
+    //         else{
+    //             p3->next=p2;
+    //             p2=p2->next;
+    //         }
            
-           p3=p3->next;
+    //        p3=p3->next;
+    //     }
+
+    //     while(p1!=NULL){
+    //         p3->next=p1;
+    //         p1=p1->next;
+    //         p3=p3->next;
+
+    //     }
+
+    //     while(p2!=NULL){
+    //         p3->next=p2;
+    //         p2=p2->next;
+    //         p3=p3->next;
+
+    //     }
+
+    //   return temp->next;
+         if(!list1){
+            return list2;
+        }
+        if(!list2){
+            return list1;
         }
 
-        while(p1!=NULL){
-            p3->next=p1;
-            p1=p1->next;
-            p3=p3->next;
+        int data1 = list1->val;
+        int data2 = list2->val;
 
+        ListNode* ans = NULL;
+        if(data1>data2){
+            ans = list2;
+            // ans->next = NULL;
+            list2 = list2->next;
+        }else{
+            ans = list1;
+            // ans->next = NULL;
+            list1 = list1->next;
         }
 
-        while(p2!=NULL){
-            p3->next=p2;
-            p2=p2->next;
-            p3=p3->next;
+        ListNode* itr = ans;
 
+        while(list1 && list2){
+            int data1 = list1->val;
+            int data2 = list2->val;
+            if(data1>data2){
+                itr->next = list2;
+                list2 = list2->next;
+            }else{
+                itr->next = list1;
+                list1 = list1->next;
+            }
+            itr = itr->next;
         }
 
-      return temp->next;
-        
+        while(list1){
+            itr->next = list1;
+            list1 = list1->next;
+            itr = itr->next;
+        }
+
+        while(list2){
+            itr->next = list2;
+            list2 = list2->next;
+            itr = itr->next;
+        }
+
+        itr->next = NULL;
+        return ans; 
     }
 };
