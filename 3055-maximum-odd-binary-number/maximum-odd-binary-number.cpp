c1384d0d0e403058673748ba3e1dc2a4f2ca73cb
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-03-01 12:28:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (29.81%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -5,7 +5,6 @@ public:
         vector<char>v(n);
         string str;
         int one=0;
-        // int n=s.size()-1;
         for(int i=0;i<n;i++){
             if(s[i]=='1'){
                 one++;
