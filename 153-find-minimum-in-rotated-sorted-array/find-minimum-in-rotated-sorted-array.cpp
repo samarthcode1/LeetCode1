094941aa79918ff20bc106e72d44fbdf3c59932c
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-03-06 18:45:30 +0530

    Time: 3 ms (57.99%) | Memory: 12.6 MB (15.75%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -6,19 +6,19 @@ public:
         int maxi=INT_MAX;
         while(low<=high){
             int mid=(low+high)/2;
-            // if(nums[mid]<maxi){
-            //     maxi=nums[mid];
-            // }
+            if(nums[mid]<maxi){
+                maxi=nums[mid];
+            }
             if(nums[low]<=nums[high]){
                 maxi=min(maxi,nums[low]);
                 return maxi;
             }
             if(nums[low]<=nums[mid]){
-                maxi=min(maxi,nums[low]);
+                // maxi=min(maxi,nums[low]);
                 low=mid+1;
             }
             else{
-                maxi=min(maxi,nums[mid]);
+                // maxi=min(maxi,nums[mid]);
                 high=mid-1;
             }
         }
