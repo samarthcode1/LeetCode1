b85d061e7552b99f909e439d1c839832b8225678
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-03-09 12:12:05 +0530

    Time: 62 ms (89.16%) | Memory: 52.9 MB (73.96%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int>mp;
-        unordered_set<int>s1,s2;
-        for(auto i:nums1){
-            s1.insert(i);
-        }
-        for(auto i:nums2){
-            s2.insert(i);
-        }
-        for(auto i:s1){
-            mp[i]++;
-        }
-        for(auto i:s2){
-            mp[i]++;
-        }
-        for(auto i:mp){
-            if(i.second>1){
-                return i.first;
+        int n1=nums1.size(),n2=nums2.size();
+        int p1=0,p2=0;
+        while(p1<n1 && p2<n2){
+            if(nums1[p1]==nums2[p2]){
+                return nums1[p1];
+            }
+            else if(nums1[p1]<nums2[p2]){
+                p1++;
+            }
+            else{
+                p2++;
             }
         }
         return -1;
+        // map<int,int>mp;
+        // unordered_set<int>s1,s2;
+        // for(auto i:nums1){
+        //     s1.insert(i);
+        // }
+        // for(auto i:nums2){
+        //     s2.insert(i);
+        // }
+        // for(auto i:s1){
+        //     mp[i]++;
+        // }
+        // for(auto i:s2){
+        //     mp[i]++;
+        // }
+        // for(auto i:mp){
+        //     if(i.second>1){
+        //         return i.first;
+        //     }
+        // }
+        // return -1;
     }
 };
