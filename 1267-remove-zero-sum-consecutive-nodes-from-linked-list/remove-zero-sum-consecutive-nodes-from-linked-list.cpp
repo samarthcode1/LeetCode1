02ab4820b67f168ab61e0b8057f5438a1025bfe6
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-03-12 22:37:35 +0530

    Time: 9 ms (42.96%) | Memory: 15.1 MB (16.76%) - LeetSync

--- 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -1,47 +1,39 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
-        ListNode* temp=new ListNode(0,head);
-        ListNode* curr=temp;
-        unordered_map<int,ListNode*>mp;
-        mp[0]=temp;
-        int totalSum=0;
-        while(curr){
-            totalSum+=curr->val;
-            mp[totalSum]=curr;
-            curr=curr->next;
+        ListNode* front = new ListNode(0, head);
+        ListNode* current = front;
+        int prefixSum = 0;
+        unordered_map<int, ListNode*> prefixSumToNode;
+        while (current != nullptr) {
+            // Add current's value to the prefix sum
+            prefixSum += current->val;
+
+            // If prefixSum is already in the hashmap, 
+            // we have found a zero-sum sequence:
+            if (prefixSumToNode.find(prefixSum) != prefixSumToNode.end()) {
+                ListNode* prev = prefixSumToNode[prefixSum];
+                current = prev->next;
+
+                // Delete zero sum nodes from hashmap
+                // to prevent incorrect deletions from linked list
+                int p = prefixSum + current->val;
+                while (p != prefixSum) {
+                    prefixSumToNode.erase(p);
+                    current = current->next;
+                    p += current->val;
+                }
+
+                // Make connection from the node before 
+                // the zero sum sequence to the node after
+                prev->next = current->next;
+            } else {
+                // Add new prefixSum to hashmap
+                prefixSumToNode[prefixSum] = current;
+            }
+            // Progress to next element in list
+            current = current->next;
         }
-        totalSum=0;
-        curr=temp;
-        while(curr){
-            totalSum+=curr->val;
-            curr->next=mp[totalSum]->next;
-            curr=curr->next;
-        }
-        return temp->next;
-
-        // int currSum=0;
-        // while(temp){
-        //     if(currSum+temp->val==0){
-        //         head=temp->next;    
-        //     }
-        //     totalSum+=temp->val;
-        //     currSum=temp->val;
-        //     if(totalSum==0){
-
-        //     }
-        //     temp=temp->next;
-
-        // }
+        return front->next;
     }
 };
