7d37e5d029f7fd78b9eed9bb646895db140b7665
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-03-15 19:03:50 +0530

    Time: 14 ms (74.02%) | Memory: 27.4 MB (34.75%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -10,19 +10,19 @@ public:
         {
             vl[i]=vl[i-1]*nums[i-1];
         }
-        for(auto i:vl){
-            cout<<i<<" ";
-        }
-        cout<<endl;
+        // for(auto i:vl){
+        //     cout<<i<<" ";
+        // }
+        // cout<<endl;
         vr[n-1]=1;
         for(int i=n-2;i>=0;i--)
         {
             vr[i]=vr[i+1]*nums[i+1];
         }
-        for(auto i:vr){
-            cout<<i<<" ";
-        }
-        cout<<endl;
+        // for(auto i:vr){
+        //     cout<<i<<" ";
+        // }
+        // cout<<endl;
         for(int i=0;i<n;i++)
         {
             product[i]=vl[i]*vr[i];
