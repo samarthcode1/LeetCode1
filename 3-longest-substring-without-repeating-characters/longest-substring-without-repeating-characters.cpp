748d33a66c9f1d5110c2e18f594f6de385ac3f83
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-03-23 16:09:09 +0530

    Time: 28 ms (20.33%) | Memory: 13.8 MB (33.84%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,32 +2,32 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n=s.length();
-        vector<int> a(200,-1);
-        // memset(a,-1,sizeof(a));
+        // vector<int> a(200,-1);
+        // // memset(a,-1,sizeof(a));
         int subsLength=0,start=-1;
-        for(int i=0;i<n;i++){
-            if(a[s[i]]>start){
-                start=a[s[i]];
-            }
-            a[s[i]]=i;
-            subsLength=max(subsLength,i-start);
-        }
-        
-        // int j=0;
-        // unordered_set<char>set;
         // for(int i=0;i<n;i++){
-        //     if(set.count(s[i])==0){
-        //         set.insert(s[i]);
-        //         subsLength=max(subsLength,i-j+1);
+        //     if(a[s[i]]>start){
+        //         start=a[s[i]];
         //     }
-        //     else{
-        //         while(set.count(s[i])){
-        //             set.erase(s[j]);
-        //             j++;
-        //         }
-        //         set.insert(s[i]);
-        //     }
+        //     a[s[i]]=i;
+        //     subsLength=max(subsLength,i-start);
         // }
+        
+        int j=0;
+        unordered_set<char>set;
+        for(int i=0;i<n;i++){
+            if(set.count(s[i])==0){
+                set.insert(s[i]);
+                subsLength=max(subsLength,i-j+1);
+            }
+            else{
+                while(set.count(s[i])){
+                    set.erase(s[j]);
+                    j++;
+                }
+                set.insert(s[i]);
+            }
+        }
         return subsLength;
 
     }
