3d32582d2df360f0fa3d208d290643d6dda24571
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-06-05 11:37:28 +0530

    Time: 10 ms (44.21%), Space: 11.9 MB (41.98%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.cpp
+++ 1044-find-common-characters/1044-find-common-characters.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
         vector<string> result;
-        vector<int> commonCount(26, INT_MAX);
+        vector<int> common(26, INT_MAX);
         for (auto word : words) {
-            vector<int> count(26, 0);
+            vector<int> v(26, 0);
             for (char c : word) {
-                count[c - 'a']++;
+                v[c - 'a']++;
             }
             for(int i = 0; i < 26; ++i) {
-                commonCount[i] = min(commonCount[i], count[i]);
+                common[i] = min(common[i], v[i]);
             }
         }
     
         for (int i = 0; i < 26; ++i) {
-            while (commonCount[i] > 0) {
+            while (common[i] > 0) {
                 result.push_back(string(1, i + 'a'));
-                commonCount[i]--;
+                common[i]--;
             }
         }
         return result;
