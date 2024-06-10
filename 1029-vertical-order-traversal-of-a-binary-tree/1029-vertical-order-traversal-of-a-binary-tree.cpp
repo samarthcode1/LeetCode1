7d83d9befd067e7212a7201b5eaeb08fe568d086
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-06-10 23:19:11 +0530

    Time: 0 ms (100%), Space: 14.8 MB (24.44%) - LeetHub

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>>v;
+        queue<pair<TreeNode*,pair<int,int>>>q;
+        map<int,map<int,multiset<int>>>mp;
+        q.push({root,{0,0}});
+        while(!q.empty()){
+            auto i=q.front();
+            q.pop();
+            TreeNode* node=i.first;
+            int x=i.second.first;
+            int y=i.second.second;
+            mp[x][y].insert(node->val);
+            if(node->left){
+                q.push({node->left,{x-1,y+1}});
+            }
+            if(node->right){
+                q.push({node->right,{x+1,y+1}});
+            }
+        }
+        for(auto i:mp){
+            vector<int>col;
+            for(auto j:i.second){
+                col.insert(col.end(),j.second.begin(),j.second.end());
+            }
+            v.push_back(col);
+        }
+        return v;
+
+    }
+};
