91c205205f427851289a2445a8389e1463991a7a
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-06-14 12:12:04 +0530

    Time: 89 ms (94.29%) | Memory: 73.9 MB (15.35%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        //1 2 3 2 3 7 
-        int count=0;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]<=nums[i-1]){
-                count+=nums[i-1]-nums[i]+1;
-                cout<<count<<" ";
-                nums[i]=nums[i-1]+1;
-                cout<<nums[i];
-            }
-            cout<<endl;
+        // sort(nums.begin(),nums.end());
+        // //1 2 3 2 3 7 
+        // int count=0;
+        // for(int i=1;i<nums.size();i++){
+        //     if(nums[i]<=nums[i-1]){
+        //         count+=nums[i-1]-nums[i]+1;
+        //         cout<<count<<" ";
+        //         nums[i]=nums[i-1]+1;
+        //         cout<<nums[i];
+        //     }
+        //     cout<<endl;
 
+        // }
+        // return count;
+        int n = nums.size();
+        int max_val = 0;
+        int minIncrements = 0;
+        for (int val : nums) {
+            max_val = max(max_val, val);
         }
-        return count;
+        vector<int> frequencyCount(n + max_val + 1, 0);
+        for (int val : nums) {
+            frequencyCount[val]++;
+        }
+        for (int i = 0; i < frequencyCount.size(); i++) {
+            if (frequencyCount[i] <= 1) continue;
+            int duplicates = frequencyCount[i] - 1;
+            frequencyCount[i + 1] += duplicates;
+            frequencyCount[i] = 1;
+            minIncrements += duplicates;
+        }
+
+        return minIncrements;
         
 
 
