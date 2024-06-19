3baab949db153f28c2bd37c9b34e53dfe0162dae
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-06-19 12:18:08 +0530

    Time: 178 ms (5.04%), Space: 73.2 MB (12.94%) - LeetHub

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        
+     std::vector<int> bloomDay1 = bloomDay;
+    std::sort(bloomDay1.begin(), bloomDay1.end());
+    int n = bloomDay.size();
+    int start = bloomDay1[0];
+    int end = bloomDay1[n - 1];
+    int res = INT_MAX;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        int count = 0;
+        int roses = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (bloomDay[i] <= mid) {
+                roses++;
+            }
+
+            if (roses == k) {
+                count++;
+                roses = 0;
+            }
+
+            if (bloomDay[i] > mid) {
+                roses = 0;
+            }
+        }
+
+        if (count >= m) {
+            res = std::min(res, mid);
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+
+    if (res ==INT_MAX ) {
+        return -1;
+    }
+
+    return res;
+}
+};
