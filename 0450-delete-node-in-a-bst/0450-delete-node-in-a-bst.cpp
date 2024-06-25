e1ac540c7bdd4794f78c4fbcb642c10f53971750
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-06-25 13:07:47 +0530

    Time: 19 ms (85.72%), Space: 30.3 MB (99.77%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>v;
+    void inorder(TreeNode* root,int key){
+        if(!root){
+            return;
+        }
+        inorder(root->left,key);
+        if(root->val!=key){
+            v.push_back(root->val);
+        }
+        inorder(root->right,key);
+    }
+    TreeNode* makeTree(int start,int end){
+        if(start>=v.size() || end<0 || start>end){
+            return nullptr;
+        }
+        TreeNode* temp=new TreeNode();
+        if(start==end){
+            temp->val=v[start];
+            return temp;
+        }
+        int mid=(start+end)/2;
+        temp->val=v[mid];
+        temp->left=makeTree(start,mid-1);
+        temp->right=makeTree(mid+1,end);
+        return temp;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        inorder(root,key);
+        int n=v.size();
+        if(!n){
+            return nullptr;
+        }
+        TreeNode* temp=new TreeNode();
+        if(n==1){
+            temp->val=v[0];
+            return temp;
+        }
+        temp->val=v[n/2];
+        temp->left=makeTree(0,(n/2)-1);
+        temp->right=makeTree((n/2)+1,n);
+        return temp;
+    }
+};
