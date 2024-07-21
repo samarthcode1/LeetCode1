3c59e0b672b31a9bdfc52e6ab32e0f73df9c39b2
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-07-21 11:29:18 +0530

    Time: 0 ms (100%), Space: 8.2 MB (66.67%) - LeetHub

---
+++ 3508-number-of-bit-changes-to-make-two-integers-equal/3508-number-of-bit-changes-to-make-two-integers-equal.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minChanges(int n, int k) {
+        if (n == k) {
+            return 0;
+        }
+
+        int count = 0;
+        while (n > 0 || k > 0) {
+            int bitN = n & 1;
+            int bitK = k & 1;
+
+            if (bitK == 1 && bitN == 0) {
+                return -1;
+            }
+
+            if (bitN == 1 && bitK == 0) {
+                count++;
+            }
+
+            n >>= 1;
+            k >>= 1;
+        }
+
+        return count;
+    }
+};
