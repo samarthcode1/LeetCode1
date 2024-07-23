4bb4820b7543a05cef4d325a1bed0a9a14f8640f
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-07-24 01:49:29 +0530

    Time: 8 ms (81.21%) | Memory: 10 MB (73.04%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,37 +1,98 @@
 class Solution {
 public:
-    string minWindow(string S, string T) {
-        string result;
-        if(S.empty() || T.empty()){
-            return result;
+    string minWindow(string s, string t) {
+        // int arr[126]={0};
+        // for(int i=0;i<t.size();i++){
+        //     int index=t[i]-'A';
+        //     arr[index]++;
+        // }
+        // int st=0;
+        // int end=0;
+        // int myarr[126]={0};
+        // string ans="";
+        // while(st<s.size() && end<s.size()){
+        //     while(end-st+1 <= t.size() && end<s.size()){
+        //         myarr[int(s[end]-'A')]++;
+        //         end++;
+        //     }
+        //     int count=0;
+        //     for(int i=0;i<126;i++){
+        //         if(arr[i]>0 && myarr[i]>=arr[i]){
+        //             count+=arr[i];
+        //         }
+        //         if(count==t.size()){
+        //             string str="";
+        //             str=s.substr(st,end);
+        //             if( str.size() <= ans.size() && ans!=""){
+        //                 ans=str;
+        //             }
+        //             else if(ans==""){
+        //                 ans=str;
+        //             }
+        //             //break;
+        //         }
+        //     }
+        //     if(count<t.size() && end<s.size()){
+        //         myarr[int(s[end]-'A')]++;
+        //         end++;
+        //     }
+        //     else if (count==t.size() && st<s.size()){
+        //         while(count==t.size() && st<end){
+        //             if(arr[s[st]-'A']>0 && myarr[ s[st]-'A']==arr[ s[st]-'A']){
+        //                 string mystr="";
+        //                 count--;
+        //                 mystr=s.substr(st,end);
+        //                 if( mystr.size() < ans.size() && ans!=""){
+        //                     ans=mystr;
+        //                 }
+        //                 //break;
+        //             }
+        //             myarr[ s[st]-'A']--;
+        //             st++;
+        //         }
+        //     }
+        // }
+        // return ans;
+        vector<int> arr(128, 0);
+        for (char c : t) {
+            arr[c]++;
         }
-        unordered_map<char, int> map;
-        unordered_map<char, int> window;
-        for(int i = 0; i < T.length(); i++){
-            map[T[i]]++;
-        }
-        int minLength = INT_MAX;
-        int letterCounter = 0;
-        int slow=0;
-        for(int fast = 0; fast < S.length(); fast++){
-            char c = S[fast];
-            if(map.find(c) != map.end()){
-                window[c]++;
-                if(window[c] <= map[c]){
-                    letterCounter++;
+    
+    // Frequency array for characters in current window in 's'
+        vector<int> myarr(128, 0);
+    
+        int start = 0, end = 0, minLen = INT_MAX, minStart = 0, count = 0;
+    
+        while (end < s.size()) {
+        // Expand the window by adding characters to the end
+            if (arr[s[end]] > 0) {
+                myarr[s[end]]++;
+                if (myarr[s[end]] <= arr[s[end]]) {
+                    count++;
                 }
             }
-            if(letterCounter >= T.length()){
-            while(map.find(S[slow]) == map.end() || window[S[slow]] > map[S[slow]]){
-                window[S[slow]]--;
-                slow++;
-            }
-            if(fast - slow + 1 < minLength){
-                minLength = fast - slow + 1;
-                result = S.substr(slow, minLength);
+            end++;
+        
+        // If current window contains all characters of 't'
+            while (count == t.size()) {
+            // Update minimum window if found smaller
+                if (end - start < minLen) {
+                    minLen = end - start;
+                    minStart = start;
+                }
+            
+            // Shrink the window from the start
+                if (arr[s[start]] > 0) {
+                    myarr[s[start]]--;
+                    if (myarr[s[start]] < arr[s[start]]) {
+                        count--;
+                    }
+                }
+                start++;
             }
         }
-    }
-    return result;
+    
+        // Return the minimum window substring
+        return minLen == INT_MAX ? "" : s.substr(minStart, minLen);
     }
 };
