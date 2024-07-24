162621f0ca838ddb4b5b1115d7df7b44e70ec28a
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-07-24 14:36:05 +0530

    Time: 394 ms (41.99%) | Memory: 88.9 MB (55.84%) - LeetSync

---
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        vector<pair<int, int>> v;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            string s = to_string(nums[i]);
+            int n = 0;
+            
+            for (char c : s) {
+                n = n * 10 + mapping[c - '0'];
+            }
+            
+            v.push_back({n, i});
+        }     
+        sort(v.begin(), v.end());
+        for(auto i:v){
+            cout<<i.first<<" "<<i.second<<endl;
+        }
+        vector<int>vec;
+        for(auto i:v){
+            vec.push_back(nums[i.second]);
+        }
+        return vec;
+    }
+};
