a626aa87283c0d3daaacb71059d277fcd6bd0834
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-08-15 18:26:02 +0530

    Time: 106 ms (5.56%) | Memory: 86.1 MB (32.71%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int f = 0, ten = 0, tw = 0;
+        for (int i = 0; i < bills.size(); i++) {
+            if (bills[i] == 5) {
+                f++;
+                cout << "Five:" << f << endl;
+            } else if (bills[i] == 10) {
+                ten++;
+                if (f <= 0) {
+                    return false;
+                }
+                f--;
+                cout << "Five:" << f << "Ten:" << ten << endl;
+            } else {
+                if(f >= 1 && ten >= 1) {
+                    f--;
+                    ten--;
+                }
+                else if(f>=3){
+                    f=f-3;
+                }
+                else{
+                    return false;
+                }
+                cout << "Five:" << f << "Ten:" << ten << endl;
+            }
+        }
+        return true;
+    }
+};
