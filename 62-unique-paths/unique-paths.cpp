29765e0b18781a0e5e3b41c5108019862283ad00
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-09-08 16:21:20 +0530

    Time: 4 ms (12.42%) | Memory: 8 MB (21.67%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-// int dp[101][101];
+    int check(int i, int j, int m, int n, vector<vector<int>>& dp) {
+        if (i == m - 1 && j == n - 1) {
+            return 1;
+        }
+        if (i > m || j > n) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        } 
+        else {
+            return dp[i][j] = check(i + 1, j, m, n, dp) + check(i, j + 1, m, n, dp);
+        }
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        for(int i=0;i<m;i++){
-            dp[i][n-1]=1;
-        }
-        for(int i=0;i<n;i++){
-            dp[m-1][i]=1;
-        }
-        for(int i=m-2;i>=0;i--){
-            for(int j=n-2;j>=0;j--){
-                dp[i][j]=dp[i+1][j]+dp[i][j+1];
-            }
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        int num = check(0, 0, m, n, dp);
+        if (m == 1 && n == 1) {
+            return num;
         }
         return dp[0][0];
     }
