6f7c82d1006225717c37ef1eac81ebe28860e001
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-11-22 23:48:37 +0530

    Time: 74 ms (10.38%) | Memory: 46.4 MB (5.06%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>& grid,int &x,int &y,int &m,int &n){
-        queue<pair<int,int>>q;
-        q.push({x,y});
-        while(!q.empty()){
-            pair p=q.front();
-            q.pop();
-            int i=p.first;
-            int j=p.second;
-            if(i-1>=0 && grid[i-1][j]=='1'){
-                grid[i-1][j]='~';
-                q.push({i-1,j});
-            }
-            if(j-1>=0 && grid[i][j-1]=='1'){
-                grid[i][j-1]='~';
-                q.push({i,j-1});
-            }
-            if(i+1<m && grid[i+1][j]=='1'){
-                grid[i+1][j]='~';
-                q.push({i+1,j});
-            }
-            if(j+1<n && grid[i][j+1]=='1'){
-                grid[i][j+1]='~';
-                q.push({i,j+1});
+    bool check(vector<vector<char>>& grid, vector<vector<bool>>& visi, int r,
+               int c) {
+        int row = grid.size();
+        int col = grid[0].size();
+
+        return (r >= 0) && (r < row) && (c >= 0) && (c < col) &&
+               (grid[r][c] == '1') && (!visi[r][c]);
+    }
+    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visi, int r,
+             int c) {
+        vector<int> X = {-1, 0,0,1};
+        vector<int> Y = {0, 1,-1,0};
+        visi[r][c] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nX = r + X[i];
+            int nY = c + Y[i];
+            if (check(grid, visi, nX, nY)) {
+                dfs(grid, visi, nX, nY);
             }
         }
-
     }
+
     int numIslands(vector<vector<char>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        int count=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='1'){
+        int r = grid.size();
+        int c = grid[0].size();
+        vector<vector<bool>> visi(r, vector<bool>(c, false));
+        int count = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == '1' && !visi[i][j]) {
+                    dfs(grid, visi, i, j);
                     count++;
-                    grid[i][j]='~';
-                    bfs(grid,i,j,m,n);
                 }
             }
         }
