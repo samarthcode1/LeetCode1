bc30749ed685d9b6b3f06ccb943d879e86cd5713
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2024-12-17 19:44:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (10.00%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        // vector<vector<int>>& m1;
-        int size = matrix.size();
-        // transpose
-        for(int i = 0; i<size; i++){
-            for(int j = i+1; j<size; j++){
-                swap(matrix[i][j], matrix[j][i]);
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<m;j++){
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
-        
-        // cout<<"Matrix is: "<<endl;
-        // for(auto i: matrix){
-        // for(auto j : i){
-        // cout<<j<<" ";
-        // }
-        // cout<<endl;
-        // }
-        // cout<<endl;
-        for(int i = 0; i<size; i++){
-            int front = 0;
-            int last = size-1;
+        for(int i=0;i<n;i++){
+            int front=0;
+            int last=n-1;
             while(front<last){
-                swap(matrix[i][front], matrix[i][last]);
-                front++;
-                last--;
+                swap(matrix[i][front],matrix[i][last]);
+                front++,last--;
             }
         }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         cout<<matrix[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
     }
 };
