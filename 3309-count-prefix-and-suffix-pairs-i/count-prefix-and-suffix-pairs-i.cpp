df77dfa27396033ef6c21cd55b3181485c7216f6
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2025-01-08 11:06:52 +0530

    Time: 136 ms (5.08%) | Memory: 24.2 MB (30.51%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -4,13 +4,10 @@ public:
         if(s2.size()<s1.size()){
             return false;
         }
-        // cout<<s1<<" "<<s2<<endl;
         string temp=s2.substr(0,s1.size());
         cout<<temp<<" ";
         int n1=s2.size()-s1.size();
         string temp1=s2.substr(n1,s2.size());
-        cout<<temp1<<" ";
-        cout<<endl;
         if((temp==s1)&&(temp1==s1)){
             return true;
         }
