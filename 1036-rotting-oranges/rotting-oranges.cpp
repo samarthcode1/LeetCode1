caec0f1c3c19bf6d4c1044237fbb857d16b0b389
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2025-01-14 15:38:53 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (49.79%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int total=0,count=0;
         queue<pair<int,int>>q;
         int dx[4]={-1,1,0,0};
-        int dy[4]={0,0,-1,1};
-        int m=grid.size(),n=grid[0].size();
-        int count=0,total=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int dy[4]={0,0,1,-1};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]!=0){
                     total++;
                 }
@@ -16,7 +17,7 @@ public:
                 }
             }
         }
-        int min=0;
+        int mini=0;
         while(!q.empty()){
             int size=q.size();
             count+=size;
@@ -28,7 +29,7 @@ public:
                 for(int k=0;k<4;k++){
                     int X=x+dx[k];
                     int Y=y+dy[k];
-                    if(X<0 || Y<0 || X>=m || Y>=n || grid[X][Y]!=1){
+                    if(X<0 || Y<0 || X>=n || Y>=m || grid[X][Y]!=1){
                         continue;
                     }
                     grid[X][Y]=2;
@@ -36,11 +37,11 @@ public:
                 }
             }
             if(!q.empty()){
-                min++;
+                mini++;
             }
         }
         if(total==count){
-            return min;
+            return mini;
         }
         return -1;
     }
