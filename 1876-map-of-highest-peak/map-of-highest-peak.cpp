509b937884699762a8460bcfd92ad937cfb6d8f0
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2025-01-22 18:41:08 +0530

    Time: 25 ms (98.30%) | Memory: 97.6 MB (99.88%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -3,29 +3,26 @@ public:
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
         int R = isWater.size();
         int C = isWater[0].size();
-        vector<vector<int>> height(R, vector<int>(C, R + C)); // Initialize with a safe sentinel value
-
-        // First pass: set water cells to 0 and propagate height from top-left to bottom-right
+        vector<vector<int>> height(R, vector<int>(C, R + C));
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
                 if (isWater[i][j] == 1) {
-                    height[i][j] = 0; // Water cell has height 0
+                    height[i][j] = 0;
                 } else {
                     if (i > 0) 
-                        height[i][j] = min(height[i][j], height[i - 1][j] + 1); // Check from top
+                        height[i][j] = min(height[i][j], height[i - 1][j] + 1);
                     if (j > 0) 
-                        height[i][j] = min(height[i][j], height[i][j - 1] + 1); // Check from left
+                        height[i][j] = min(height[i][j], height[i][j - 1] + 1);
                 }
             }
         }
 
-        // Second pass: propagate height from bottom-right to top-left
         for (int i = R - 1; i >= 0; i--) {
             for (int j = C - 1; j >= 0; j--) {
                 if (i < R - 1) 
-                    height[i][j] = min(height[i][j], height[i + 1][j] + 1); // Check from bottom
+                    height[i][j] = min(height[i][j], height[i + 1][j] + 1); 
                 if (j < C - 1) 
-                    height[i][j] = min(height[i][j], height[i][j + 1] + 1); // Check from right
+                    height[i][j] = min(height[i][j], height[i][j + 1] + 1); 
             }
         }
 
