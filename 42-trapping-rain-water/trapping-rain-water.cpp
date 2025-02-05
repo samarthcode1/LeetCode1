dc82f53a6318493c5750ee57f176f62775f86281
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2025-02-05 13:36:05 +0530

    Time: 0 ms (100.00%) | Memory: 27 MB (44.07%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -4,17 +4,15 @@ public:
         stack<int>st;
         int n=height.size();
         int ans=0;
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             while(!st.empty() && height[st.top()]<height[i]){
-                int top=st.top();
+                int val=st.top();
                 st.pop();
-                if(st.empty())
-                {
+                if(st.empty()){
                     break;
                 }
                 int length=i-st.top()-1;
-                ans+=(min(height[st.top()],height[i])-height[top])*length;
+                ans+=(min(height[st.top()],height[i])-height[val])*length;
             }
             st.push(i);
         }
