cdb6293f5a1a75761386539827fa471d66b249df
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2025-02-07 02:05:54 +0530

    Time: 3 ms (65.52%) | Memory: 10.2 MB (57.33%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,58 +1,50 @@
 class Solution {
 public:
-    bool isSafe(vector<string> v,int n,int col,int row){
-        int r=row;
-        int c=col;
-        //up Diag
+    bool isSafe(int col,int n,vector<string>& v,int row){
+        int r=row,c=col;
         while(r>=0 && c>=0){
             if(v[r][c]=='Q'){
                 return false;
             }
-            r--;
-            c--;
+            r--,c--;
         }
-        //left
-        r=row;
-        c=col;
+        r=row,c=col;
         while(c>=0){
             if(v[r][c]=='Q'){
                 return false;
             }
             c--;
         }
-        //bottom dig
-        r=row;
-        c=col;
+        r=row,c=col;
         while(r<=n-1 && c>=0){
             if(v[r][c]=='Q'){
                 return false;
             }
-            r++;
-            c--;
+            r++,c--;
         }
         return true;
     }
-    void check(vector<string>& v,int n,int col,vector<vector<string>>& queen){
-        if(n==col){
-            queen.push_back(v);
+    void check(int col,int n,vector<string>& v,vector<vector<string>>& ans){
+        if(col==n){
+            ans.push_back(v);
             return;
         }
         for(int i=0;i<n;i++){
-            if(isSafe(v,n,col,i)){
+            if(isSafe(col,n,v,i)){
                 v[i][col]='Q';
-                check(v,n,col+1,queen);
+                check(col+1,n,v,ans);
                 v[i][col]='.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>queen;
+        vector<vector<string>>ans;
         vector<string>v(n);
         string s(n,'.');
         for(int i=0;i<n;i++){
             v[i]=s;
         }
-        check(v,n,0,queen);
-        return queen;
+        check(0,n,v,ans);
+        return ans;
     }
 };
