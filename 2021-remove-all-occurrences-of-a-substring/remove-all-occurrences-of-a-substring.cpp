61cfc89e7793278acc797ff382ad337edb035d21
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2025-02-11 12:33:27 +0530

    Time: 15 ms (5.85%) | Memory: 29 MB (5.32%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool check(stack<char>st,string part,int n1){
+        // stack<char>st=temp;
+        for(int i=n1-1;i>=0;i--){
+            if(st.top()!=part[i]){
+                return false;
+            }
+            st.pop();
+        }
+        return true;
+    }
+    string removeOccurrences(string s, string part) {
+        int n1=part.size();
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            st.push(s[i]);
+            if(st.size()>=n1 && check(st,part,n1)){
+                for(int j=0;j<n1;j++){
+                    st.pop();
+                }
+            }
+        }
+        string s1="";
+        while(!st.empty()){
+            s1=st.top()+s1;
+            st.pop();
+        }
+        // reverse(s.begin(),s.end());
+        return s1;
+
+    }
+};
