1fd43172908a9db534bc586bcaee37dd6eff25aa
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2025-02-11 12:34:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (68.15%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -11,23 +11,26 @@ public:
         return true;
     }
     string removeOccurrences(string s, string part) {
-        int n1=part.size();
-        stack<char>st;
-        for(int i=0;i<s.size();i++){
-            st.push(s[i]);
-            if(st.size()>=n1 && check(st,part,n1)){
-                for(int j=0;j<n1;j++){
-                    st.pop();
-                }
-            }
+        // int n1=part.size();
+        // stack<char>st;
+        // for(int i=0;i<s.size();i++){
+        //     st.push(s[i]);
+        //     if(st.size()>=n1 && check(st,part,n1)){
+        //         for(int j=0;j<n1;j++){
+        //             st.pop();
+        //         }
+        //     }
+        // }
+        // string s1="";
+        // while(!st.empty()){
+        //     s1=st.top()+s1;
+        //     st.pop();
+        // }
+        // // reverse(s.begin(),s.end());
+        // return s1;
+        while(s.length()>0 && s.length()>s.find(part)){
+           s.erase(s.find(part),part.length());
         }
-        string s1="";
-        while(!st.empty()){
-            s1=st.top()+s1;
-            st.pop();
-        }
-        // reverse(s.begin(),s.end());
-        return s1;
-
+        return s;
     }
 };
