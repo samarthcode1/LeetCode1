3a7706a0a50c5c6e29dd322435b0f1fcaed19a7a
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2025-03-31 23:18:11 +0530

    Time: 0 ms (100.00%) | Memory: 94 MB (31.52%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
@@ -1,21 +1,21 @@
 class Solution {
- public:
-  long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-    long long ans = 0;
-    int j = -1;
-    int prevMinKIndex = -1;
-    int prevMaxKIndex = -1;
-
-    for (int i = 0; i < nums.size(); ++i) {
-      if (nums[i] < minK || nums[i] > maxK)
-        j = i;
-      if (nums[i] == minK)
-        prevMinKIndex = i;
-      if (nums[i] == maxK)
-        prevMaxKIndex = i;
-      ans += max(0, min(prevMinKIndex, prevMaxKIndex) - j);
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        long long count=0;
+        int mini=-1,maxi=-1;
+        int j=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<minK || nums[i]>maxK){
+                j=i;
+            }
+            if(nums[i]==minK){
+                mini=i;
+            }
+            if(nums[i]==maxK){
+                maxi=i;
+            }
+            count+=max(0,min(maxi,mini)-j);
+        }
+        return count;
     }
-
-    return ans;
-  }
 };
