1cab4bb776398f5b2a44adff85087b4288b9b374
Samarth Nag <96525920+samarthcode1@users.noreply.github.com>
2025-04-04 17:02:25 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (74.04%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void check(int idx,int sum,int n,vector<int>& v,vector<vector<int>>& ans,vector<int>& candidates,int target){
-        if(idx==n){
-            if(sum==target){
-                ans.push_back(v);
+    void find(int idx, int sum, vector<vector<int>>& res, vector<int>& v, int n,
+              vector<int>& candidates, int target) {
+        if (idx == n) {
+            if (sum == target) {
+                res.push_back(v);
             }
             return;
         }
-        if(sum+candidates[idx]<=target){
-            sum+=candidates[idx];
+
+        if (sum + candidates[idx] <= target) {
             v.push_back(candidates[idx]);
-            check(idx,sum,n,v,ans,candidates,target);
-            sum-=candidates[idx];
+            find(idx, sum + candidates[idx], res, v, n, candidates, target);
             v.pop_back();
         }
-        check(idx+1,sum,n,v,ans,candidates,target);
+        find(idx + 1, sum, res, v, n, candidates, target);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
-        vector<int>v;
-        int n=candidates.size();
-        check(0,0,n,v,ans,candidates,target);
-        return ans;
+        vector < vector<int>> res;
+        vector<int> v;
+        int n = candidates.size();
+        find(0, 0, res, v, n, candidates, target);
+        return res;
     }
 };
